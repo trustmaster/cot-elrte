13fd4986cf1fa880091c29cb777d2f872b0004a8
Vladimir Sibirov <trustmaster@kodigy.com>
2012-07-12 16:24:14 +0400

    Fix for IE8.

--- elrte/elrte.editor.php
+++ elrte/elrte.editor.php
@@ -28,16 +28,18 @@ if (!file_exists($mkup_lang))
 }
 
 // Load resources
-$mkup_skin = cot_rc('code_rc_css_file', array('url' => $cfg['plugins_dir'] . '/elrte/css/smoothness/jquery-ui-1.8.20.custom.css'));
-$mkup_theme = cot_rc('code_rc_css_file', array('url' => $cfg['plugins_dir'] . '/elrte/css/elrte.min.css'));
+$mkup_skin_path = $cfg['plugins_dir'] . '/elrte/css/smoothness/jquery-ui-1.8.20.custom.css';
+$mkup_skin = cot_rc('code_rc_css_file', array('url' => $mkup_skin_path));
+$mkup_theme_path = $cfg['plugins_dir'] . '/elrte/css/elrte.min.css';
+$mkup_theme = cot_rc('code_rc_css_file', array('url' => $mkup_theme_path));
 cot_rc_link_footer($cfg['plugins_dir'] . '/elrte/js/jquery-ui-1.8.20.custom.min.js');
 cot_rc_link_footer($cfg['plugins_dir'] . '/elrte/js/elrte.min.js');
 cot_rc_link_footer($mkup_lang);
 cot_rc_link_footer($cfg['plugins_dir'] . "/elrte/js/elrte.set.js");
 
 cot_rc_embed_footer('$(document).ready(function() {
-	$("head").append(\''.$mkup_skin.'\');
-	$("head").append(\''.$mkup_theme.'\');
+	if (document.createStyleSheet) { document.createStyleSheet("'.$mkup_skin_path.'"); } else { $("head").append("'.$mkup_skin.'"); }
+	if (document.createStyleSheet) { document.createStyleSheet("'.$mkup_theme_path.'"); } else { $("head").append("'.$mkup_theme.'"); }
 	mySettings.lang = \''.$lang_mark.'\';mediSettings.lang = \''.$lang_mark.'\';miniSettings.lang = \''.$lang_mark.'\';
 	$("textarea.editor").elrte(mySettings);
 	$("textarea.medieditor").elrte(mediSettings);
